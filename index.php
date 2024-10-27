d7d00479ba924e13a15c0cd4748481a49a766b1a
BugBun <ziadcy04@gmail.com>
2024-10-27 01:41:49 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Page title</title>
+</head>
+<body>
+    <script>alert(111);</script>
+</body>
+</html>
+
+?>
